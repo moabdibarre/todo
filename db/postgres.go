88e30701e00ed81ee968b90d3eefb00e19a8e634
mohamed <mohamed@Khadijas-MBP.communityfibre.co.uk>
2025-07-25 18:46:02 +0100

    feat 3 Todos

--- db/postgres.go
+++ db/postgres.go
@@ -17,7 +17,7 @@ func InitDB() {
 	// create connection str
 	// connStr := "user=postgres password=password dbname=tasks port=5432 sslmode=disable"
 	connStr := fmt.Sprintf(
-		"host=%s user =%s password=%s dbname=%s port=%s sslmode=disable",
+		"host=%s usergit=%s password=%s dbname=%s port=%s sslmode=disable",
 		os.Getenv("DB_HOST"),
 		os.Getenv("DB_USER"),
 		os.Getenv("DB_PASSWORD"),
